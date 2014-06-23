daf28374b626472e52653d2f6f7608aa6e454445
Legokichi Duckscallion <legokichi@duxca.com>
2014-06-23 12:24:07 +0900

    a

--- module.js
+++ module.js
@@ -181,19 +181,19 @@ build = function(_arg, _arg1, _arg2, callback) {
     styles = [];
     scripts = [];
     if (enableFirebugLite) {
-      scripts.push("https://altjsdoit.github.io/thirdparty/firebug/firebug-lite.js#overrideConsole=true,showIconWhenHidden=true,startOpened=true,enableTrace=true");
+      scripts.push("./thirdparty/firebug/firebug-lite.js#overrideConsole=true,showIconWhenHidden=true,startOpened=true,enableTrace=true");
     }
     if (enableFirebugLite) {
       js.code = "try{" + js.code + "}catch(err){console.error(err);console.error(err.stack);}";
     }
     if (enableJQuery) {
-      scripts.push("https://altjsdoit.github.com/thirdparty/jquery/jquery.min.js");
+      scripts.push("./thirdparty/jquery/jquery.min.js");
     }
     if (enableUnderscore) {
-      scripts.push("https://altjsdoit.github.com/thirdparty/underscore.js/underscore-min.js");
+      scripts.push("./thirdparty/underscore.js/underscore-min.js");
     }
     if (enableES6shim) {
-      scripts.push("https://altjsdoit.github.com/thirdparty/es6-shim/es6-shim.min.js");
+      scripts.push("./thirdparty/es6-shim/es6-shim.min.js");
     }
     if ((js.err != null) || (html.err != null) || (css.err != null)) {
       return callback(buildHTML({
