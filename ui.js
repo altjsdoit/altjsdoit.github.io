be9ab6fe41107342256d8d82d454aa59a33b23bc
Legokichi Duckscallion <legokichi@duxca.com>
2014-06-27 18:06:23 +0900

    update

--- ui.js
+++ ui.js
@@ -5,9 +5,14 @@ $(function() {
     $("#main").find(".active").removeClass("active");
     return $("#main").find("#" + $(ev.target).attr("data-open")).addClass("active");
   });
-  return $("#menuLink").click(function() {
+  $("#menuLink").click(function() {
     $("#layout").toggleClass("active");
     $("#menu").toggleClass("active");
     return $("#menuLink").toggleClass("active");
   });
+  if ($(window).width() > 640) {
+    $("#layout").toggleClass("active");
+    $("#menu").toggleClass("active");
+    return $("#menuLink").toggleClass("active");
+  }
 });
